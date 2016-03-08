99c7d5775730d9dd91efdcc3c6b8ce3b47278ffd
Paris de Perez Escudero <pdepesc@gmail.com>
2016-03-08 21:41:10 +0100

    Primera versión con todos los directorios
    Dos 'soluciones' de Visual Studio:
    - EvaluacionSistema (buena)
    - Pruebas (para hacer pruebas con librerias etc)

---
+++ Programacion/EvaluacionSistema/EvaluacionSistema/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenHardwareMonitor;
+using OpenHardwareMonitor.Hardware;
+
+namespace EvaluacionSistema
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Computer myPc = new Computer() { CPUEnabled=true, FanControllerEnabled=true, GPUEnabled=true, HDDEnabled=true, MainboardEnabled=true, RAMEnabled=true };
+
+            myPc.Open();
+
+            String tab = "\t";
+
+            foreach (var hardware in myPc.Hardware)
+            {
+                hardware.Update();
+                Console.WriteLine(hardware);
+                Console.WriteLine(tab + hardware.Name);
+                Console.WriteLine(tab + hardware.HardwareType);
+                Console.WriteLine(tab + hardware.Identifier);
+                Console.WriteLine(tab + hardware.Sensors + " - " + hardware.Sensors.Length);
+                Console.WriteLine(tab + hardware.SubHardware + " - " + hardware.SubHardware.Length);
+                getSubHardware(hardware, tab);
+            }
+            
+            Console.Read();
+
+        }
+
+        //llamada recursiva para explorar el arbol de hardware y subhardware
+        public static void getSubHardware(IHardware hardware, String tab)
+        {
+            tab = tab + "\t";
+            foreach (IHardware subhardware in hardware.SubHardware)
+            {
+                subhardware.Update();
+                Console.WriteLine(subhardware);
+                Console.WriteLine(tab + subhardware.Name);
+                Console.WriteLine(tab + subhardware.HardwareType);
+                Console.WriteLine(tab + subhardware.Identifier);
+                Console.WriteLine(tab + subhardware.Parent.Name);
+                Console.WriteLine(tab + subhardware.Sensors + " - " + subhardware.Sensors.Length);
+                Console.WriteLine(tab + subhardware.SubHardware + " - " + subhardware.SubHardware.Length);
+                getSubHardware(subhardware, tab);
+            }
+        }
+    }
+}
---
+++ Programacion/Pruebas/Pruebas/Contadores.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pruebas
+{
+    class Contadores
+    {
+        public static void getCategorias()
+        {
+            PerformanceCounterCategory[] categorias;
+            // Retrieve the categories.
+            categorias = PerformanceCounterCategory.GetCategories();
+            // Add the retrieved categories to the list. 
+            using (System.IO.StreamWriter file = new System.IO.StreamWriter(@"C:\Users\Public\PerformanceCounters.txt"))
+            {
+                for (int i = 0; i < categorias.Length; i++)
+                {
+                    file.WriteLine("------------------------------------------------------");
+                    file.WriteLine(categorias[i].CategoryName.ToString());
+                    file.WriteLine("------------------------------------------------------");
+                    getContadores(categorias[i].CategoryName.ToString(), file);
+                }
+            }
+        }
+
+        public static void getContadores(String categoria, System.IO.StreamWriter file)
+        {
+            string[] instanceNames;
+            ArrayList counters = new ArrayList();
+            PerformanceCounterCategory mycat = new PerformanceCounterCategory(categoria);
+            // Retrieve the counters.
+            try
+            {
+                instanceNames = mycat.GetInstanceNames();
+                if (instanceNames.Length == 0)
+                {
+                    foreach (PerformanceCounter counter in mycat.GetCounters())
+                    {
+                        var unused = counter.NextValue(); // first call will always return 0
+                        //System.Threading.Thread.Sleep(1000); // wait a second, then try again
+                        file.WriteLine(counter.CounterName.ToString() + ": " + counter.NextValue() + " - " + counter.NextValue());
+                    }
+                }
+                else
+                {
+                    for (int i = 0; i < instanceNames.Length; i++)
+                    {
+                        file.WriteLine("---INSTANCIA " + i + ": " + instanceNames[i] + "---");
+                        foreach (PerformanceCounter counter in mycat.GetCounters(instanceNames[i]))
+                        {
+                            var unused = counter.NextValue(); // first call will always return 0
+                            //System.Threading.Thread.Sleep(10); // wait a second, then try again
+                            file.WriteLine(counter.CounterName.ToString() + ": " + counter.NextValue() + " - " + counter.NextValue());
+                        }
+                    }
+                }
+            }
+            catch (System.Exception ex)
+            {
+                file.WriteLine(
+                    "Unable to list the counters for this category:\n"
+                    + ex.Message);
+            }
+        }
+    }
+}
